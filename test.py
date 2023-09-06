6e0e51c579629480a5d63733edc0eeedd9c64c53
AngieeeMo <88923292+AngieeeMo@users.noreply.github.com>
2023-09-05 21:34:10 -0600

    Update test.py
    comentado

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-import mnist_loader
+import mnist_loader ## importar código/bloque de los datos
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper() 
 training_data = list(training_data) 
-import network 
+import network #importar código de la red
  
-net = network.Network([784, 30, 10]) 
+net = network.Network([784, 30, 10]) ##parametros para entrenar
 net.SGD(training_data, 55, 10, 0.07, test_data=test_data)
